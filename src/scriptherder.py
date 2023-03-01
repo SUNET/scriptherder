ccb788212cf3471f1d38af316a1e6af6f7c41846
Fredrik Thulin <fredrik@thulin.net>
2023-03-01 11:12:57 +0100

    bump copyright

--- src/scriptherder.py
+++ src/scriptherder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2014, 2015, 2017, 2018 SUNET. All rights reserved.
+# Copyright 2014, 2015, 2017, 2018, 2023 SUNET. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are
 # permitted provided that the following conditions are met:
